d0b197919583b9de3a41a10e0c788e403790f088
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2014-06-23 23:45:32 +0200

    Bump version to 0.2

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object BuildSettings {
 
   val buildSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "com.softwaremill.macmemo",
-    version := "0.1",
+    version := "0.2",
     scalacOptions ++= Seq(),
     scalaVersion := ScalaVersion,
     // Sonatype OSS deployment
