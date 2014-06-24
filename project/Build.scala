40edda84c12ac9b2aa061dd2f9cd1bbb20dfaf9a
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2014-06-24 17:12:55 +0200

    Start 0.3-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object BuildSettings {
 
   val buildSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "com.softwaremill.macmemo",
-    version := "0.2",
+    version := "0.3-SNAPSHOT",
     scalacOptions ++= Seq(),
     scalaVersion := ScalaVersion,
     // Sonatype OSS deployment
